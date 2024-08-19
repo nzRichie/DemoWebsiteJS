07af8ddbf1cc183bdd694c90089de5b0187cced5
nzRichie <richiewlarsen@gmail.com>
2024-08-20 11:36:45 +1200

    Tested and applied Prettier to format the code, assignment ready for submission

--- domScripts.js
+++ domScripts.js
@@ -1,157 +1,157 @@
 // HANDLE PAGE CHANGES
 
-prevPage = document.getElementById('previousPage');
-nextPage = document.getElementById('nextPage');
+prevPage = document.getElementById("previousPage");
+nextPage = document.getElementById("nextPage");
 
 let pages = [];
 let pageIndex = 0;
 
 // Assign each page to a list of divs
-for (let i = 1; i < 6; i++){
-    page = document.getElementById(('page-' + i));
-    if (i != 1){
-        page.style.display = 'none';
-    }
-    pages.push(page);
+for (let i = 1; i < 6; i++) {
+  page = document.getElementById("page-" + i);
+  if (i != 1) {
+    page.style.display = "none";
+  }
+  pages.push(page);
 }
 
-prevPage.style.visibility = 'hidden';
-
+// Hide the previous page button on initial load
+prevPage.style.visibility = "hidden";
 
 // Next page onclick function
-nextPage.onclick = function(){
+nextPage.onclick = function () {
+  // Hide the current page
+  pages[pageIndex].style.display = "none";
 
-    // Hide the current page
-    pages[pageIndex].style.display = 'none';
+  // If we were on the first page, show the previous page button.
+  if (pageIndex == 0) {
+    prevPage.style.visibility = "visible";
+  }
 
-    // If we were on the first page, show the previous page button.
-    if (pageIndex == 0){
-        prevPage.style.visibility = 'visible';
-    }
+  // Increment the counter and show the next page
+  pageIndex++;
+  pages[pageIndex].style.display = "flex";
 
-    // Increment the counter and show the next page
-    pageIndex++;
-    pages[pageIndex].style.display = 'flex';
-
-    // If we arrived on the last page, hide the next page button.
-    if (pageIndex == 4){
-        calculateTotalScore();
-        nextPage.style.visibility = 'hidden';
-        calculateTotalScore();
-    }
-    handleProgressBar();
-}
+  // If we arrived on the last page, hide the next page button.
+  if (pageIndex == 4) {
+    calculateTotalScore();
+    nextPage.style.visibility = "hidden";
+    calculateTotalScore();
+  }
+  handleProgressBar();
+};
 
 // Previous page onclick function
-prevPage.onclick = function(){
+prevPage.onclick = function () {
+  // Hide the current page
+  pages[pageIndex].style.display = "none";
 
-    // Hide the current page
-    pages[pageIndex].style.display = 'none';
+  // If we were on the last page, show the next page button
+  if (pageIndex == 4) {
+    nextPage.style.visibility = "visible";
+  }
 
-    // If we were on the last page, show the next page button
-    if (pageIndex == 4){
-        nextPage.style.visibility = 'visible';
+  // decrement page index and display the previous page
+  pageIndex--;
+  pages[pageIndex].style.display = "flex";
+
+  // If we arrived on the first page, hide the previous page button.
+  if (pageIndex == 0) {
+    prevPage.style.visibility = "hidden";
+  }
+
+  handleProgressBar();
+};
+
+// Changes the progress bar colours to reflect the current page
+function handleProgressBar() {
+  for (let i = 1; i < 6; i++) {
+    progressBarDivs = document.getElementsByClassName("p" + i);
+    for (let j = 0; j < progressBarDivs.length; j++) {
+      if (i <= pageIndex + 1) {
+        progressBarDivs[j].style.backgroundColor = "#b45dbb";
+      } else {
+        progressBarDivs[j].style.backgroundColor = "#a8a8a8";
+      }
     }
-
-    // decrement page index and display the previous page
-    pageIndex--;
-    pages[pageIndex].style.display = 'flex';
-
-    // If we arrived on the first page, hide the previous page button.
-    if (pageIndex == 0){
-        prevPage.style.visibility = 'hidden';
-    }
-    handleProgressBar();
-}
-
-function handleProgressBar(){
-    for (let i = 1; i < 6; i++){
-        progressBarDivs = document.getElementsByClassName('p' + i);
-        for (let j = 0; j < progressBarDivs.length; j++){
-            if (i <= pageIndex + 1){
-                progressBarDivs[j].style.backgroundColor = "#b45dbb";
-            }
-            else {
-                progressBarDivs[j].style.backgroundColor = "#a8a8a8";
-            }
-        }
-    }
+  }
 }
 // PAGE ONE SCRIPTS
 
 // Gender select handling
 
-maleButton = document.getElementById('maleButton');
-femaleButton = document.getElementById('femaleButton');
+maleButton = document.getElementById("maleButton");
+femaleButton = document.getElementById("femaleButton");
 
-maleButton.onclick = function() {
-    if (!male){
-        femaleButton.style.backgroundColor = "#a8a8a8";
-        maleButton.style.backgroundColor = "#b45dbb";
-        male = true;
-    }
+// male buttons clicked, change the gender variable and set the colours of the two buttons
+maleButton.onclick = function () {
+  if (!male) {
+    femaleButton.style.backgroundColor = "#a8a8a8";
+    maleButton.style.backgroundColor = "#b45dbb";
+    male = true;
+  }
 };
 
-femaleButton.onclick = function() {
-    if (male){
-        femaleButton.style.backgroundColor = "#b45dbb";
-        maleButton.style.backgroundColor = "#a8a8a8";
-        male = false;
-    }
+// female buttons clicked, change the gender variable and set the colours of the two buttons
+femaleButton.onclick = function () {
+  if (male) {
+    femaleButton.style.backgroundColor = "#b45dbb";
+    maleButton.style.backgroundColor = "#a8a8a8";
+    male = false;
+  }
 };
 
 // Slider handling
 
-ageRangeSlider = document.getElementById('ageSlider');
-ageSpan = document.getElementById('ageSpan');
+ageRangeSlider = document.getElementById("ageSlider");
+ageSpan = document.getElementById("ageSpan");
 ageSpan.innerHTML = ageRangeSlider.value;
 
-ageRangeSlider.oninput = function(){
-    ageSpan.innerHTML = this.value;
-    age = this.value;
-}
-// PAGE TWO SCRIPTS
-
+// Change the age displayed in the span based on input from the slider
+ageRangeSlider.oninput = function () {
+  ageSpan.innerHTML = this.value;
+  age = this.value;
+};
 
 // PAGE THREE SCRIPTS
 
-doesSmoke = document.getElementById('smoker-button-true');
-doesntSmoke = document.getElementById('smoker-button-false');
+// Variables for the two buttons
+doesSmoke = document.getElementById("smoker-button-true");
+doesntSmoke = document.getElementById("smoker-button-false");
 
-function smokerButtonClick(){
-    if (!smoker){
-        doesntSmoke.style.backgroundColor = "#b45dbb";
-        doesSmoke.style.backgroundColor = "#a8a8a8";
-    }
-    else {
-        doesntSmoke.style.backgroundColor = "#a8a8a8";
-        doesSmoke.style.backgroundColor = "#b45dbb";
-    }
+// Changes which button is highlighted based on user input
+function smokerButtonClick() {
+  if (!smoker) {
+    doesntSmoke.style.backgroundColor = "#b45dbb";
+    doesSmoke.style.backgroundColor = "#a8a8a8";
+  } else {
+    doesntSmoke.style.backgroundColor = "#a8a8a8";
+    doesSmoke.style.backgroundColor = "#b45dbb";
+  }
 }
 
 // PAGE FOUR SCRIPTS
 
-treatedButton = document.getElementById('treated-button-true');
-untreatedButton = document.getElementById('treated-button-false');
+treatedButton = document.getElementById("treated-button-true");
+untreatedButton = document.getElementById("treated-button-false");
 
-function treatedButtonClick(){
-    if (!BPtreated){
-        untreatedButton.style.backgroundColor = "#b45dbb";
-        treatedButton.style.backgroundColor = "#a8a8a8";
-    }
-    else {
-        untreatedButton.style.backgroundColor = "#a8a8a8";
-        treatedButton.style.backgroundColor = "#b45dbb";
-    }
+function treatedButtonClick() {
+  if (!BPtreated) {
+    untreatedButton.style.backgroundColor = "#b45dbb";
+    treatedButton.style.backgroundColor = "#a8a8a8";
+  } else {
+    untreatedButton.style.backgroundColor = "#a8a8a8";
+    treatedButton.style.backgroundColor = "#b45dbb";
+  }
 }
 
 // PAGE FIVE SCRIPTS
 
-displayHeader = document.getElementById('risk-display');
-scoreHeader = document.getElementById('score-display');
+displayHeader = document.getElementById("risk-display");
+scoreHeader = document.getElementById("score-display");
 
-function displayScore(risk, score){
-    displayHeader.innerHTML = risk;
-    scoreHeader.innerHTML = "Final Score: " + score;
-
+// Display final score in the span element dedicated to that.
+function displayScore(risk, score) {
+  displayHeader.innerHTML = risk;
+  scoreHeader.innerHTML = "Final Score: " + score;
 }
--- framinghamCalc.js
+++ framinghamCalc.js
@@ -1,10 +1,21 @@
-
 // Lookup table for both smoking and cholesterol age ranges
 const cholSmokAgeRanges = new Array(20, 40, 50, 60, 70);
 
 // Lookup tables for Cholesterol
-const femaleCholesterolScores = [[0, 4, 8, 11, 13], [0, 3, 6, 8, 10], [0, 2, 4, 5, 7], [0, 1, 2, 3, 4], [0, 1, 1, 2, 2]];
-const maleCholesterolScore = [[0, 4, 7, 9, 11], [0, 3, 5, 6, 8], [0, 2, 3, 4, 5], [0, 1, 1, 2, 3], [0, 0, 0, 1, 1]];
+const femaleCholesterolScores = [
+  [0, 4, 8, 11, 13],
+  [0, 3, 6, 8, 10],
+  [0, 2, 4, 5, 7],
+  [0, 1, 2, 3, 4],
+  [0, 1, 1, 2, 2],
+];
+const maleCholesterolScore = [
+  [0, 4, 7, 9, 11],
+  [0, 3, 5, 6, 8],
+  [0, 2, 3, 4, 5],
+  [0, 1, 1, 2, 3],
+  [0, 0, 0, 1, 1],
+];
 
 // Lookup tables for smokers
 const femaleSmokerScores = [9, 7, 4, 2, 1];
@@ -25,10 +36,42 @@ const maleTBPscores = [0, 1, 2, 2, 3];
 const maleUTBPscores = [0, 0, 1, 1, 2];
 
 // Lookup tables for total scores and percentages
-const femaleTotalRanges = [-30, 9, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const femaleTotalRanges = [
+  -30, 9, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+];
 const maleTotalRanges = [-30, 1, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-const tenYearRiskFemale = ["< 1%", "1%", "2%", "3%", "4%", "5%", "6%", "8%", "11%", "14%", "17%", "22%", "27%", "> 30%"];
-const tenYearRiskMale = ["< 1%", "1%", "2%", "3%", "4%", "5%", "6%", "8%", "10%", "12%", "16%", "20%", "25%", "> 30%"];
+const tenYearRiskFemale = [
+  "< 1%",
+  "1%",
+  "2%",
+  "3%",
+  "4%",
+  "5%",
+  "6%",
+  "8%",
+  "11%",
+  "14%",
+  "17%",
+  "22%",
+  "27%",
+  "> 30%",
+];
+const tenYearRiskMale = [
+  "< 1%",
+  "1%",
+  "2%",
+  "3%",
+  "4%",
+  "5%",
+  "6%",
+  "8%",
+  "10%",
+  "12%",
+  "16%",
+  "20%",
+  "25%",
+  "> 30%",
+];
 
 let male = true;
 let smoker = false;
@@ -41,156 +84,149 @@ let score = 0;
 let riskPercent = 0;
 
 // Function for settings various values called by buttons etc.
-function setCholesterolIndex(index){
-    cholesterolIndex = index;
+function setCholesterolIndex(index) {
+  cholesterolIndex = index;
 }
 
-function setHDLCholesterolIndex(index){
-    HDLCholesterolIndex = index;
+function setHDLCholesterolIndex(index) {
+  HDLCholesterolIndex = index;
 }
 
-function setSmokerValue(smokes){
-    smoker = smokes;
-    smokerButtonClick();
+function setSmokerValue(smokes) {
+  smoker = smokes;
+  smokerButtonClick();
 }
 
-function setBPIndex(index){
-    bloodPressureIndex = index;
+function setBPIndex(index) {
+  bloodPressureIndex = index;
 }
 
-function setBPTreated(treated){
-    BPtreated = treated;
-    treatedButtonClick();
+function setBPTreated(treated) {
+  BPtreated = treated;
+  treatedButtonClick();
 }
 
 // Calculates the total score
-function calculateTotalScore(){
+function calculateTotalScore() {
+  score = 0;
 
-    score = 0;
-
-    score += calculateAgeScore();
-    score += calculateBPScore();
-    score += calculateCholesterol();
-    score += calculateSmokerScore();
-    score += calculateHDLScore();
-    riskPercent = calculateRisk();
-    displayScore(riskPercent, score);
+  score += calculateAgeScore();
+  score += calculateBPScore();
+  score += calculateCholesterol();
+  score += calculateSmokerScore();
+  score += calculateHDLScore();
+  riskPercent = calculateRisk();
+  displayScore(riskPercent, score);
 }
 
 // return the percent risk based on the score
-function calculateRisk(){
-    let i = 0;
+function calculateRisk() {
+  let i = 0;
 
-    // risk for males
-    if (male == true){
-        while (i < maleTotalRanges.length - 2){
-            if (score < maleTotalRanges[i + 1] && score >= maleTotalRanges[i]){
-                if (i > 0){
-                    i--;
-                }
-                break;
-            }
-            i++;
+  // risk for males
+  if (male == true) {
+    while (i < maleTotalRanges.length - 2) {
+      if (score < maleTotalRanges[i + 1] && score >= maleTotalRanges[i]) {
+        if (i > 0) {
+          i--;
         }
-        return tenYearRiskMale[i];
+        break;
+      }
+      i++;
     }
-    // risk for females
-    while (i < femaleTotalRanges.length - 2){
-        if (score < femaleTotalRanges[i + 1] && score >= femaleTotalRanges[i]){
-            if (i > 0){
-                i--;
-            }
-            break;
-        }
-        i++;
+    return tenYearRiskMale[i];
+  }
+  // risk for females
+  while (i < femaleTotalRanges.length - 2) {
+    if (score < femaleTotalRanges[i + 1] && score >= femaleTotalRanges[i]) {
+      if (i > 0) {
+        i--;
+      }
+      break;
     }
-    return tenYearRiskFemale[i];
-
+    i++;
+  }
+  return tenYearRiskFemale[i];
 }
 
 // Return score based on gender, blood-pressure, and treated status
-function calculateBPScore(){
-
-    // for males
-    if (male == true){
-        if (BPtreated == true){
-            return maleTBPscores[bloodPressureIndex];
-        }
-        return maleUTBPscores[bloodPressureIndex];
+function calculateBPScore() {
+  // for males
+  if (male == true) {
+    if (BPtreated == true) {
+      return maleTBPscores[bloodPressureIndex];
     }
+    return maleUTBPscores[bloodPressureIndex];
+  }
 
-    // for females
-    if (BPtreated == true){
-        return femaleTBPscores[bloodPressureIndex];
-    }
-    return femaleUTBPscores[bloodPressureIndex];
+  // for females
+  if (BPtreated == true) {
+    return femaleTBPscores[bloodPressureIndex];
+  }
+  return femaleUTBPscores[bloodPressureIndex];
 }
 
 // Return the score based on HDL cholesterol
-function calculateHDLScore(){
-
-    return HDLscores[HDLCholesterolIndex];
+function calculateHDLScore() {
+  return HDLscores[HDLCholesterolIndex];
 }
 
 // Return score based on age and gender
-function calculateAgeScore(){
-    
-    let i = 0;
-    // Use the age to find the "age range"
-    while(i < ageRanges.length - 1){
-        if (age < ageRanges[i + 1] && age >= ageRanges[i]){
-            break;
-        }
-        i++;
+function calculateAgeScore() {
+  let i = 0;
+  // Use the age to find the "age range"
+  while (i < ageRanges.length - 1) {
+    if (age < ageRanges[i + 1] && age >= ageRanges[i]) {
+      break;
     }
+    i++;
+  }
 
-    // return the male or female scores
-    if (male == true){
-        return maleAgeScores[i];
-    }
-    return femaleAgeScores[i];
+  // return the male or female scores
+  if (male == true) {
+    return maleAgeScores[i];
+  }
+  return femaleAgeScores[i];
 }
 
 // return score based on age, gender, and smoking status
-function calculateSmokerScore(){
-    
-    // Return zero if subject doesn't smoke
-    if (smoker == false){
-        return 0;
-    }
-    
-    // Find the appropriate age range for table lookup.
-    let i = 0;
-    while(i < cholSmokAgeRanges.length - 1){
-        if (age < cholSmokAgeRanges[i + 1] && age >= cholSmokAgeRanges[i]){
-            break;
-        }
-        i++;
-    }
+function calculateSmokerScore() {
+  // Return zero if subject doesn't smoke
+  if (smoker == false) {
+    return 0;
+  }
 
-    // return the male or female score
-    if (male == true){
-        return maleSmokerScores[i];
+  // Find the appropriate age range for table lookup.
+  let i = 0;
+  while (i < cholSmokAgeRanges.length - 1) {
+    if (age < cholSmokAgeRanges[i + 1] && age >= cholSmokAgeRanges[i]) {
+      break;
     }
-    return femaleSmokerScores[i];
+    i++;
+  }
+
+  // return the male or female score
+  if (male == true) {
+    return maleSmokerScores[i];
+  }
+  return femaleSmokerScores[i];
 }
 
 // return score based on age, gender, and cholesterol
-function calculateCholesterol(){
+function calculateCholesterol() {
+  let i = 0;
 
-    let i = 0;
-
-    // find the appropriate age index for lookup
-    while(i < cholSmokAgeRanges.length - 1){
-        if (age < cholSmokAgeRanges[i + 1] && age >= cholSmokAgeRanges[i] ){
-            break;
-        }
-        i++;
+  // find the appropriate age index for lookup
+  while (i < cholSmokAgeRanges.length - 1) {
+    if (age < cholSmokAgeRanges[i + 1] && age >= cholSmokAgeRanges[i]) {
+      break;
     }
+    i++;
+  }
 
-    // return the male or female score
-    if (male == true){
-        return maleCholesterolScore[i][cholesterolIndex];
-    }
-    return femaleCholesterolScores[i][cholesterolIndex];
+  // return the male or female score
+  if (male == true) {
+    return maleCholesterolScore[i][cholesterolIndex];
+  }
+  return femaleCholesterolScores[i][cholesterolIndex];
 }
